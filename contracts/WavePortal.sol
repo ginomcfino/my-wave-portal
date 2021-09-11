5d7f62683d73a7253b39d8b6112f60d2a40896f8
Weiqi Ji <jiweiqi717@gmail.com>
2021-09-10 22:01:53 -0400

    waving to myself!

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,8 +5,18 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint totalWaves;
     constructor() {
-        console.log("AYYY gues who just made a contract!");
+        console.log("AYYY gues who just made a smart contract!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s is the contract waved!", msg.sender);
+    }
+
+    function getTotalWaves()  view public returns (uint) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
     }
 }
-
